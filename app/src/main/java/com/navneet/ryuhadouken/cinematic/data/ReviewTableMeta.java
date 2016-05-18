d904c35762a3afb27ed7d93f260dfb1152ed5ccb
navneet sharma <navneetnnavneet1@gmail.com>
2016-05-18 17:47:44 +0530

    Added Cinematic app

---
+++ app/src/main/java/com/navneet/ryuhadouken/cinematic/data/ReviewTableMeta.java
@@ -0,0 +1,17 @@
+package com.navneet.ryuhadouken.cinematic.data;
+
+/**
+ * Created by ryuhadouken on 17/5/16.
+ */
+public interface ReviewTableMeta {
+
+    String MOVIE_ID = "movie_id";
+    String ID = "_id";
+    String AUTHOR = "_author";
+    String CONTENT = "content";
+    String URL = "url";
+
+    String[] COLUMNS = {"movie_id","_id","_author","content","url"};
+
+    int MOVIE_ID_ID = 0, ID_ID = 1, AUTHOR_ID = 2, CONTENT_ID = 3, URL_ID = 4;
+}
